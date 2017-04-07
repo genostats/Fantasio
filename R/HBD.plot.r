3b31d51d79484cc4004d5d5c27ea042d54abbd5c
Isuru <isuru.liyanage.20@gmail.com>
2017-04-07 15:07:16 +0200

    add HBD.plot function for training and HFLOD (not finished yet)

---
+++ R/HBD.plot.r
@@ -0,0 +1,125 @@
+HBD.plot <- function(x)
+{ 
+  
+  ###############################
+  #       graph per chr         #
+  ###############################
+  
+  
+  dir.create("Plots")#creation du dossier Plots contenant les sorties
+  last <- function(x) { return (length(x))}
+ 
+  
+  for ( i in unique(x@map$chr)) # pour chaque chr on fait un plot 
+  {
+      r <- NULL
+      #Obtenir les distances
+      d <- x@map$distance[x@map$chr == i]
+      n <- last(d)
+      
+      
+      #plotting
+      ymax <- max(3, max(x@FLOD[, x@map$chr == i], na.rm = TRUE)) 
+      xmax <- max(d, na.rm = TRUE)
+      ymin <- min(x@FLOD[, x@map$chr == i], na.rm = TRUE)
+      png(file = paste("Plots/FLOD.","cM.",i,".png",sep = ""), width = 2400, height = 1000, pointsize=24)
+          
+      #d=distance pour chaque chr i ; r=FLOD pour chaque Indiv j & chr i
+      plot(d, r, type="n", pch=16, xlim = c(0,xmax), ylim = c(ymin, ymax), xlab = "Position (cM)", ylab="FLOD", cex.lab=1.4, cex.axis=1.5, main= paste("FLOD (chromosome ",i,")", sep=""), cex.main=1.5)    
+      
+      for ( m in 1:ymax)
+      {
+        abline(h=m, col="grey", lwd=1.5, lty=2)
+      }
+      
+      abline(h=3, col="grey", lwd=2)
+      
+      
+      #Obtenir les FLOD de chaque individu
+      for ( j in 1:x@nrow )
+        
+      {
+        if(x@f[j] == 0) next
+        r <- x@FLOD[j, x@map$chr == i]
+        lines(d, r,col= j)
+      } 
+      
+      
+                  
+      
+      
+      dev.off()
+  }  
+    
+  ############################
+  #      Manhattan plot      #
+  ############################
+  
+  #prendre la derniere position du chr i
+  n <- 0
+  res <- c(x@map$distance[x@map$chr == 1])
+  res_pos <- c()
+  
+  for ( i in unique(x@map$chr))
+  {
+    d <- x@map$distance[x@map$chr == i]
+    n2 <- last(d)
+    
+   
+    # ajouter a toutes les positions du chr i + 1 la derniere valeurs 
+    #de la pos du chr i
+   
+    d <- x@map$distance[x@map$chr == i+1] + n
+    v <- d[last(n2)]
+    n <- n + x@map$distance[n2]
+    res <- c(res,d)
+    res_pos <- c(res_pos, v)
+  }
+  res_pos <- res_pos[!is.na(res_pos)]
+  last_res <- last(res)
+  h <- last(res_pos)
+  res_pos[h +1] <- res[last_res] # utile pour tracer les lignes
+  
+  
+  ymax <- max(x@FLOD, na.rm = TRUE)
+  ymin <- min(x@FLOD, na.rm=TRUE)
+  xmax <- max(x@map$distance, na.rm=TRUE)
+  mycol <- rep(c("cadetblue2",8),11) #couleur des points
+  
+  #Plotting : tous les chr pour un individus 
+  png(file = paste("Plots/FLOD.", "cM.","png",sep=""), width = 2400, height = 800, pointsize = 24 )
+  for ( i in x@nrow)
+  {
+    if(x@f[j] == 0) next
+    plot(res, x@FLOD[i,], type="p", pch=16, xlim=c(0,xmax), ylim = c(ymin,ymax), xlab = "", ylab = "FLOD", cex.lab = 1.4, cex.axis=1.5, col= mycol[unique(x@map$chr)], xaxt="n", cex=0.75 )  #
+    #lines(res,x@FLOD[i,],col=2,lwd=3) # demande des valeurs finie  
+    #for(i in unique(x@map$chr)) 
+    #{
+    #  abline(v=x@map$distance[],col="grey",lwd=2);
+    #  axis(1,at=mean(chr_pos[i:(i+1)]),i,col.ticks=0,cex.axis=1.5)
+    #}
+    #abline(v=chr_pos[23],col="grey",lwd=2);  #
+    #for ( i in 1:ymax)
+    #{
+    #  abline(h=i, col="grey", lwd=1.5, lty=2)
+    #}  
+    #abline(h=3,col="grey",lwd=2)
+    
+    
+    #pour chaque chr les separer par des lignes
+    n <- NULL
+    for ( j in unique(x@map$chr))
+    {
+      d <- x@map$distance[x@map$chr == j]
+      n <- last(d)
+      d <- x@map$distance[x@map$chr == j+1] + n
+      abline(h=x@map$distance[n],col="grey",lwd=2)
+      
+      n <- n + x@map$distance[n2]
+    }
+    axis(1,at=res_pos,1:22,col.ticks=0,cex.axis=1.5) 
+  }
+  
+  dev.off()
+    
+}
--- R/set.HFLOD.r
+++ R/set.HFLOD.r
@@ -7,28 +7,55 @@ set.HFLOD <- function(x, step = 0.01)
   while(nb * step < 1)
   {
     alpha[i] <- nb * step
-    nb = nb + 1
-    i = i + 1
+    nb <- nb + 1
+    i <- i + 1
   }
   alpha[i] <- 1
   
   
-  # calculer les HFLOD 
-  HFLOD <- matrix(0.0, nrow = x@ncol, ncol = x@nrow) #en ligne les marqueurs en colonnes les individus ??? 
-  sum_HFLOD <- 0 
-  last <- function(x) { return( length(x) ) } #obtenir le dernier elements de alpha
+  # calculer les HFLOD pour chaque marqueurs 
+  HFLOD <- matrix(0.0, nrow = x@ncol, ncol = 2) #en ligne les marqueurs en colonnes HFLOD et alpha
   
+#######################################################################################################################################  
+
+  temp <- matrix(alpha)
+  #sum_HFLOD <- 0 
+  
+  ####### obtenir les FLOD pour l'individus i 
+  #ind <- x@nrow
+  #p <- 0
+  #all_FLOD <- NULL
+  
+   
+
+  last <- function(x) { return( length(x) ) } #obtenir le dernier elements de alpha
+  FLOD_ind <- 0
   for (i in 1:x@ncol)
   {
-    i <- 0
-    while(i != last(alpha))
-    {
-      x <- log10(alpha[i] * exp(x@FLOD[i] * log(10)) + (1 - alpha[i]))
-      sum_HFLOD = sum_HFLOD + x
+      FLOD_ind <- x@FLOD[,i]#FLOD pour le marqueurs i
+      j <- 1
+      g <- 0
+      while(j <= last(alpha))
+      {
+          g <- temp[j] * (exp(FLOD_ind * log(10))-1)+1
+          temp[j] <- max(g, 0.0001, na.rm = TRUE)
+          temp[j] <- log(temp[j])/log(10)
+          
+          for(v in 1:last(temp))
+          {
+            HFLOD[i] <- HFLOD[i] + temp[i]
+          }
+         j <- j + 1
+          
+          
+        
+    
+      }
     }
-    HFLOD[i,1:x@nrow] <- sum_HFLOD
-  }
+    
+   
   
+
   
   x@HFLOD <- HFLOD 
   return(x)
