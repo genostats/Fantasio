242d9bbca486f883f15507e6c5f20b48592d3f38
IsuruHaupe <isuru.liyanage.20@gmail.com>
2017-07-07 14:20:40 +0200

    +parallel on submaps

--- R/submap.summary.r
+++ R/submap.summary.r
@@ -3,9 +3,10 @@ submap.summary <- function(h, a.threshold = 1)
   f <-  sapply(h, function(x) x@f) 
   a <-  sapply(h, function(x) x@a)
   p <- sapply(h , function(x) x@p.lrt)
-  f[a > a.threshold] <- NA
-  a[a > a.threshold] <- NA
-  p[a > a.threshold] <- NA
+  w.a <- (a > a.threshold)
+  f[w.a] <- NA
+  p[w.a] <- NA
+  a[w.a] <- NA
   pLRT_MEDIAN <- apply(p, 1, median, na.rm=TRUE)
   
   l <- p < 0.05
@@ -16,20 +17,31 @@ submap.summary <- function(h, a.threshold = 1)
     nValidSubmap <- c(nValidSubmap, sum(l[i,], na.rm = TRUE ))
   }
   
+  #treat the case whe quality is equal to NA
+  quality <- (rowSums(sapply(h, function(x) x@a < a.threshold) )/length(h))*100
+  n <- which(is.na(quality))
+  quality[n] <- 0
+  
   
   df <- data.frame(FID           = h[[1]]@ped$famid, 
                    IID           = h[[1]]@ped$id,
                    STATUS        = h[[1]]@ped$pheno,
-                   SUBMAPS       = length(h),
-                   QUALITY       = rowSums(sapply(h, function(x) x@a < a.threshold) )/length(h),
+                   SUBMAPS       = length(h),#cat(length(h), "/", length(h))
+                   QUALITY       = quality,
                    F_MIN         = apply(f, 1, min, na.rm = TRUE), 
                    F_MAX         = apply(f, 1, max, na.rm = TRUE),
                    F_MEAN        = apply(f, 1, mean, na.rm = TRUE),
                    F_MEDIAN      = apply(f, 1, median, na.rm=TRUE),
                    A_MEDIAN      = apply(a, 1, median, na.rm=TRUE),
                    pLRT_MEDIAN   = pLRT_MEDIAN, 
-                   INBRED        = pLRT_MEDIAN < 0.05, 
+                   INBRED        = as.integer(pLRT_MEDIAN < 0.05), 
                    LRT_inf_0.05  = nValidSubmap)
+  
+  for(i in 1:nrow(df))
+  {
+    if(!is.finite(df$F_MEDIAN[i]))
+      df[i,5:13] <- NA
+  }
   
   #write.table(df,"dataframe.txt",sep="\t",col.names = colnames(df))
   #library("xlsx", character.only = TRUE)
--- R/submaps.r
+++ R/submaps.r
@@ -1,16 +1,42 @@
 # paralleliser !!
-submaps <- function(x, n = 100, segments, epsilon = 1e-3, run.festim = TRUE, verbose = TRUE) {
+submaps <- function(x, n = 100, segments = segments(x), n.cores = 1, epsilon = 1e-3, run.festim = TRUE, probs = TRUE,  verbose = TRUE) {
 
-  submap <- vector("list", n)
-  for ( i in 1:n)
-  { 
+  ff <- function(i, run.festim) {
     set.seed(i)  # FOR DEBUG PURPOSE TO BE REMOVED!!!
-    if(verbose) cat("creating submap number : ", i,"/", n, "\n" )
     spider <- createSubmap(x, segments, epsilon) 
     if(run.festim) 
-      spider <- festim(spider)
-    submap[[i]] <- spider
+      spider <- festim(spider, probs = probs)
+    spider
   }
+
+  if(n.cores != 1 & .Platform$OS.type != "unix") {
+    warning("FORK cluster unavailable only one core used")
+    n.cores <- 1
+  }
+  
+  if(n.cores == 1) {
+    submap <- lapply(1:n, ff, run.festim = run.festim)
+  } else {
+    RNGkind("L'Ecuyer-CMRG")
+    s <- matrix(.Random.seed, nrow = 1)
+    for(i in 2:n.cores) 
+      s <- rbind(s, nextRNGStream(s[i-1,]))
+    cl <- makeForkCluster(n.cores)
+    parLapply(cl, 1:n.cores, function(i) .Random.seed <<- s[i,] )
+    submap <- parLapply(cl, 1:n, ff, run.festim = run.festim)
+    stopCluster(cl)
+  }
+  
+  #submap <- vector("list", n)
+  #for ( i in 1:n)
+  #{ 
+  #  set.seed(i)  # FOR DEBUG PURPOSE TO BE REMOVED!!!
+  #  if(verbose) cat("creating submap number : ", i,"/", n, "\n" )
+  #  spider <- createSubmap(x, segments, epsilon) 
+  #  if(run.festim) 
+  #    spider <- festim(spider)
+  #  submap[[i]] <- spider
+  #}
 
   if(verbose) cat("Creation of all the Submaps over ! \n")
   return(submap)
