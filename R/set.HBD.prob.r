a8a31c6749711b6411419ea1ef5066830e574428
IsuruHaupe <isuru.liyanage.20@gmail.com>
2017-07-10 09:55:03 +0200

    delete FLOD.prob and update set.HBD.prob

--- R/set.HBD.prob.r
+++ R/set.HBD.prob.r
@@ -14,19 +14,19 @@ set.HBD.prob <- function(x)
     }
   }
   
-  HBD_prob <- matrix(NA, nrow= sum(l > 0), ncol = x@ncol)#matrice contenant les probabilités pour les individus
+  HBD_prob <- matrix(NA, nrow= sum(l > 0), ncol = x@ncol)#matrice contenant les probabilites pour les individus
   #dimnames(HBD_prob) <- list(rownames(HBD_prob, do.NULL = FALSE, prefix = "individual_"), colnames(HBD_prob) <- c(x@map$id))
   
   for (i in 1:nrow(HBD_prob))
   {
     if(!is.na(x@a[i]) & !is.na(x@f[i]))#a laisser ??? 
     {
-      if(l[[i]] == 0)
+      if(!l[[i]] == 0)
       {
-        next()#if the number at this position is equal to 0 pass the test
+        HBD_prob[i,1:x@ncol] <-forward.backward(get.log.emiss(x, i), x@delta.dist, x@a[i], x@f[i] )[2,]
       }
         
-      HBD_prob[i,1:x@ncol] <-forward.backward(get.log.emiss(x, i), x@delta.dist, x@a[i], x@f[i] )[2,]
+      
     }
       
   }
