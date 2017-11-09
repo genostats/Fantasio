b3363ebacfdecab2dd80fe08e094ded30208fcfc
IsuruHaupe <isuru.liyanage.20@gmail.com>
2017-11-09 11:48:01 +0100

    HBD recap by segments

--- R/HBD.recap.r
+++ R/HBD.recap.r
@@ -1,7 +1,30 @@
-HBD.recap <- function(h)
+HBD.recap <- function(h, by_segments=F)
 {
-  proba <- submap.HBD(h)
+  proba <- submap.HBD(h)#recuperer les probas HBD pour chaque sous cartes sous forme d'une liste
 
+  if(by_segments)
+  {
+    nom <- unique(unlist(sapply(proba, function(x) rownames(x)))) #recuperer le nom de chaque individus apparut
+    matrice <- matrix(NA, nrow=length(nom), ncol=ncol(proba[[1]]))#creer une matrice avec les individus en lignes 
+                                                                  #et les segments en colonnes
+    rownames(matrice) <- nom
+    Sum <- 0
+    for(j in 1 : length(nom))
+    {
+      cpt <- 0
+      for(i in 1:length(proba))
+      {
+        line <- which(nom[j] == rownames(proba[[i]]))
+        if(length(line) == 0) next()
+        Sum <- Sum + proba[[i]][line, ]
+        cpt <- cpt + 1 
+      }
+      matrice[j, ] <- Sum / cpt
+      
+    }
+    return(matrice)
+  }
+  
   ##tableau comptant le nombre de fois ou un marqueur a ete selectionne
   marqueurs <- as.data.frame(table(unlist(sapply(proba, function(x) colnames(x)))), stringsAsFactors=FALSE)
   
@@ -27,10 +50,7 @@ HBD.recap <- function(h)
   #trouver l'/les indice(s) et la/les sous-carte(s) avec des valeurs pour chaque marqueur
   az <- vapply(proba, function(jj) match(marqueurs[,1], colnames(jj)), integer(length(marqueurs[,1])))
   
-  #boucle de remplissage de la matrice
-  #Sys.setenv("PKG_CXXFLAGS"="-std=c++11")
-  #.Call('hbdRecap', PACKAGE = "FEstim", az, matrice, nb_probs, proba)
-  
+ 
   M <- sapply(proba, function(pr) match(rownames(pr), rownames(matrice)))
   
   
@@ -41,7 +61,6 @@ HBD.recap <- function(h)
     v <- sapply(I, function(i) proba[[i]][, az1[i]], simplify = F)
     for( j in 1:length(v))
     {
-      # m <- match(rownames(as.data.frame(v[[j]])), rownames(matrice))
       m <- M[[ I[j] ]]
       matrice[m,i] <- matrice[m,i] + v[[j]]
       nb_probs[m,i] <- nb_probs[m,i] + 1
