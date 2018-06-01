320a8616edca7d51a6d89f491ec236535434835a
HervePerdry <herve.perdry@gmail.com>
2018-06-01 10:42:38 +0200

    corrected bug in set.HBD.prob

--- R/set.HBD.prob.r
+++ R/set.HBD.prob.r
@@ -1,26 +1,16 @@
-#x an matrix with a and f computed 
-# a modifier condition
-set.HBD.prob <- function(x, condition = which(x@a <= 1))
-{
-  id    <- c(x@ped$id[condition])
-  famid <- c(x@ped$famid[condition])
+# x a matrix with a and f computed 
+set.HBD.prob <- function(x, which.inds = which(x@a <= 1)) {
+  id    <- c(x@ped$id[which.inds])
+  famid <- c(x@ped$famid[which.inds])
   
-  namevector <- c()
+  HBD_prob <- matrix(NA, nrow = length(which.inds), ncol = x@ncol)#HBD matrix
+  rownames(HBD_prob) <- id
+  colnames(HBD_prob) <- c(x@map$id)
   
-  for(i in 1:length(id))
-  {
-    namevector <- c(namevector, id[i])
-  }
-  
-  HBD_prob <- matrix(NA, nrow = length(condition), ncol = x@ncol)#HBD matrix
-  dimnames(HBD_prob) <- list(rownames(HBD_prob) <- namevector, colnames(HBD_prob) <- c(x@map$id))
-  
-  for (i in 1:nrow(HBD_prob))
-  {
-    if(!is.na(x@a[condition[i]]) & !is.na(x@f[condition[i]]))
-    {
-        HBD_prob[i,1:x@ncol] <-forward.backward(get.log.emiss(x, i), x@delta.dist, x@a[condition[i]], x@f[condition[i]] )[2,]
-    }
+  for (i in 1:nrow(HBD_prob)) {
+    i1 <- which.inds[i]
+    if(!is.na(x@a[i1]) & !is.na(x@f[i1]))
+      HBD_prob[i,1:x@ncol] <-forward.backward(get.log.emiss(x, i1), x@delta.dist, x@a[i1], x@f[i1] )[2,]
   }
   x@HBD.prob <- HBD_prob 
   return(x)
