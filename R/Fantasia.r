12bae5a8a90769f75d729a873c4f925ea021890f
IsuruHaupe <isuru.liyanage.20@gmail.com>
2018-07-19 15:35:42 +0200

    removed a test to use global variable

--- R/Fantasia.r
+++
@@ -1,9 +0,0 @@
-#' \code{Fantasio} package
-#'
-#'
-#' @docType package
-#' @name Fantasio
-NULL
-
-## quiets concerns of R CMD check re: the .'s that appear in pipelines
-if(getRversion() >= "2.15.1") utils::globalVariables(c("."))
