a1115ae7b2b97e7fd4c059c319675a8e53af0fb3
HervePerdry <herve.perdry@gmail.com>
2019-03-14 13:03:11 +0100

    hotspot agregation at the end of chr did not keep the right ordering

--- R/cleanHotspots.r
+++ R/cleanHotspots.r
@@ -1,17 +1,15 @@
 cleanHotspots <- function(l, number_of_marker)
 {
   index <- c()
-  for(i in 1:length(l))
-  {
-    if(length(l[[i]]) < number_of_marker)
-    {
+  for(i in 1:length(l)) {
+    if(length(l[[i]]) < number_of_marker) {
       if(i == length(l))
-        l[[i-1]] <- c(l[[i]], l[[i-1]])
+        l[[i-1]] <- c(l[[i-1]], l[[i]])
       else
         l[[i+1]] <- c(l[[i]], l[[i+1]])
-      }else{
-        index <- c(index, i)
-      }
+      } else {
+      index <- c(index, i)
+    }
   }
   l <- l[index]
   l
