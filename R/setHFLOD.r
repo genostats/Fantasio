74d1f23602432ce35482ba46b81ce051e9de3ed6
Margot Derouin <margot.derouin@inserm.fr>
2022-02-07 12:04:23 +0100

    use getPostition() in setHFLOD()

--- R/setHFLOD.r
+++ R/setHFLOD.r
@@ -16,48 +16,7 @@
 #' @export
 setHFLOD <- function(atlas, w.id)
 {
-  if(class(atlas@submaps_list[[1]])[1] != "snpsMatrix" & class(atlas@submaps_list[[1]])[1] != "HostspotsMatrix")
-    stop("need either an hotspots.segments list of submaps or a snpsSegments list of submaps.") 
-  
-  if(class(atlas@bedmatrix)[1] != "bed.matrix") 
-    stop("Need a bed.matrix.")
-  
-  if(atlas@bySegments) {
-    index <- sapply(atlas@submaps_list, function(i) i@submap) #index of the marker
-    
-    poscM <- matrix(0.0, nrow = nrow(index), ncol = ncol(index))
-    posBp <- matrix(0.0, nrow = nrow(index), ncol = ncol(index))
-    
-    poscM_mean <- numeric(nrow(index))
-    posBp_mean <- numeric(nrow(index))
-    
-    for(i in seq_len(nrow(index))) {
-      for(j in seq_len(ncol(index)))#get position of the marker
-      {
-        poscM[i, j] <- atlas@bedmatrix@snps$dist[index[i,j]]
-        posBp[i, j] <- atlas@bedmatrix@snps$pos[index[i,j]]
-      }
-      #calculate mean value
-      poscM_mean[i] <- mean(poscM[i,])
-      posBp_mean[i] <- mean(posBp[i,])
-    }
-    
-    HFLOD <- data.frame(CHR    = atlas@submaps_list[[1]]@map$chr, 
-                        pos_cM = poscM_mean, 
-                        pos_Bp = posBp_mean)
-    
-  } else {
-    names       <- colnames(atlas@FLOD_recap)  # les ids des marqueurs où on a le FLOD
-    index       <- match(names, atlas@bedmatrix@snps$id)
-    distance_cM <- atlas@bedmatrix@snps$dist[index]
-    distance_bP <- atlas@bedmatrix@snps$pos[index]
-    chr         <- atlas@bedmatrix@snps$chr[index]
-    
-    HFLOD <- data.frame(CHR    = chr, 
-                        SNPS   = names, 
-                        pos_cM = distance_cM,
-                        pos_Bp = distance_bP)
-  }
+  HFLOD <- getPosition(atlas, atlas@FLOD_recap)
 
   HFLOD_value <- numeric(nrow(HFLOD))
   ALPHA_value <- numeric(nrow(HFLOD))
