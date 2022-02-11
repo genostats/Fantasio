dd9586a4ec8a6d70027e822d67f5779e223915d4
Margot Derouin <margot.derouin@inserm.fr>
2022-02-11 11:55:53 +0100

    phenotype conversion

--- R/phenoConverter.r
+++ R/phenoConverter.r
@@ -6,7 +6,7 @@
 #' R : 0:control ; 1:cases ; NA:unknown (default)
 #' plink : 1:control ; 2:cases ; 0/-9/NA:unknown
 
-phenoConverter = function (x, phen.code = 'R') {
+phenoConverter = function (x, phen.code) {
 	if (phen.code == 'plink') {
 		x@ped$pheno <- ifelse(x@ped$pheno == 1, 0, ifelse(x@ped$pheno == 2, 1, NA))
 	} else continue
