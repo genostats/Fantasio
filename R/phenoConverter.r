854efd6292c528bafbe6eae54052d206583869a9
HervePerdry <herve.perdry@gmail.com>
2022-02-11 14:36:01 +0100

    phenoConverter / corrected case phen.code = "R"

--- R/phenoConverter.r
+++ R/phenoConverter.r
@@ -6,9 +6,10 @@
 #' R : 0:control ; 1:cases ; NA:unknown (default)
 #' plink : 1:control ; 2:cases ; 0/-9/NA:unknown
 
-phenoConverter = function (x, phen.code = 'R') {
-	if (phen.code == 'plink') {
-		x@ped$pheno <- ifelse(x@ped$pheno == 1, 0, ifelse(x@ped$pheno == 2, 1, NA))
-	} else continue
-	}
-	
+phenoConverter = function (x, phen.code = c("R", "plink")) {
+  phen.code <- match.arg(phen.code) 
+  if (phen.code == 'plink') 
+    ifelse(x@ped$pheno == 1, 0, ifelse(x@ped$pheno == 2, 1, NA))
+  else 
+    x@ped$pheno
+}
