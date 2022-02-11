298add570da19d82396cd40e8ad1f9642f3c7d3e
HervePerdry <herve.perdry@gmail.com>
2022-02-11 14:52:32 +0100

    corrected phenoConverter (its call in Fantasio had been modified in the interval)

--- R/phenoConverter.r
+++ R/phenoConverter.r
@@ -5,11 +5,11 @@
 #' @param phen.code type of coding phenotype 'R' or 'plink'
 #' R : 0:control ; 1:cases ; NA:unknown (default)
 #' plink : 1:control ; 2:cases ; 0/-9/NA:unknown
+#' @return the bed matrix with converted phenotype
 
 phenoConverter = function (x, phen.code = c("R", "plink")) {
   phen.code <- match.arg(phen.code) 
   if (phen.code == 'plink') 
-    ifelse(x@ped$pheno == 1, 0, ifelse(x@ped$pheno == 2, 1, NA))
-  else 
-    x@ped$pheno
+    x@ped$pheno <- ifelse(x@ped$pheno == 1, 0, ifelse(x@ped$pheno == 2, 1, NA))
+  x
 }
