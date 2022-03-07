c67539e93327c0634df7981c69c8592e59672946
Margot Derouin <margot.derouin@inserm.fr>
2022-03-07 14:23:53 +0100

    add treshold- Bonferroni correction

--- R/glmHBDPlot.r
+++ R/glmHBDPlot.r
@@ -36,6 +36,8 @@ glmHBDPlot = function ( x, expl_var, save = FALSE) {
 	dev.new(height = 5.1, width = 5, ypos = 650)
 	print(p2)
 	
+	treshold = -log10(0.05/dim(x@submaps_list[[1]])[2])  #treshold based on the number of markers
+	
 	# Manhattan Plot
 	# Change colnames to fit with gaston
 	colnames(unadj)[colnames(unadj) == 'pos_Bp'] <- 'pos'
@@ -43,16 +45,15 @@ glmHBDPlot = function ( x, expl_var, save = FALSE) {
 	
 	dev.new(width = 14.25, height= 5.1, ypos = 0, xpos = 640)
 	gaston::manhattan(unadj, main = paste("Manhattan Plot \n GLM with ", expl_var, "- unadjusted data"), chrom.col = c("darksalmon", "darkturquoise"))
-	#abline(h=treshold, col = 'red')
+	abline(h=treshold, col = 'red')
 	
 	colnames(adj)[colnames(adj) == 'pos_Bp'] <- 'pos'
 	colnames(adj)[colnames(adj) == 'p_value'] <- 'p'
 	
 	# adjusted
-	#treshold = -log10(1/dim(x@submaps_list[[1]])[2])  #treshold based on the number of markers
 	dev.new(width = 14.25, height= 5.1, ypos = 650, xpos = 640)
 	gaston::manhattan(adj, main = paste("Manhattan Plot \n GLM with ", expl_var, "- adjusted data"), chrom.col = c("darksalmon", "darkturquoise"))
-	#abline(h=treshold, col = 'red')
+	abline(h=treshold, col = 'red')
 	
 	
 	} else { # save plots in png files 
