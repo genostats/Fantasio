e95bdf813b526077e674de56801d4c3994a67564
HervePerdry <herve.perdry@gmail.com>
2023-01-06 17:49:49 +0100

    rewrote fonction cleanHotspots

--- R/cleanHotspots.r
+++ R/cleanHotspots.r
@@ -1,16 +1,22 @@
-cleanHotspots <- function(l, number_of_marker)
-{
+cleanHotspots <- function(l, minMarkers) {
   index <- c()
-  for(i in seq_along(l)) {
-    if(length(l[[i]]) < number_of_marker) {
-      if(i == length(l))
-        l[[i-1]] <- c(l[[i-1]], l[[i]])
-      else
-        l[[i+1]] <- c(l[[i]], l[[i+1]])
+  i <- 1L
+  while(i <= length(l)) {
+    if(1L + diff(l[[i]]) < minMarkers) {
+      if(i == length(l)) { # dernier segment
+        # on fusionne (i-1) et i, on vire i, on termine
+        l[[i-1L]] <- c(l[[i-1L]][1L], l[[i]][2L])
+        l[[i]] <- NULL
+        return(l)
       } else {
-      index <- c(index, i)
+        # on fusionne i et i+1,  on vire i
+        l[[i+1L]] <- c(l[[i]][1L], l[[i+1L]][2L])
+        l[[i]] <- NULL
+      }
+    } else { # on n'incrémente i que si il n'y a pas eu fusion de i et i+1 !
+      i <- i + 1L
     }
   }
-  l <- l[index]
   l
 }
+
