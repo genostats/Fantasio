b7287421312281dea9fac5d928bac165068f9ec4
Margot Derouin <margot.derouin@inserm.fr>
2023-01-13 11:10:43 +0100

    more specific message when nrow(HBD)==0

--- R/plotHBDSegmentsId.r
+++ R/plotHBDSegmentsId.r
@@ -20,7 +20,7 @@ plotHBDSegmentsId <- function(Submaps, unit= "cM", id, famid, regions, outfile, 
   
   HBD.recap <- Submaps@HBD_recap
   HBDsegments <- Submaps@HBDsegments
-
+  
   # individuals_name <- rownames(HBD.recap)#get the name of the individual
   # individuals_name <- strsplit(individuals_name, "_")
   # individuals_name <- sapply(individuals_name, function(i) match(i[2], Submaps@bedmatrix@ped$id))
@@ -28,16 +28,21 @@ plotHBDSegmentsId <- function(Submaps, unit= "cM", id, famid, regions, outfile, 
   # individuals_name <- Submaps@bedmatrix@ped$id[individuals_name]
   
   # id   <- which(individuals_name == id)
-
+  
   # if(length(id) == 0)
   #  stop("No individual found")
   
   HBDsegments_rbind <- do.call(rbind, HBDsegments) #binding lines 
   
   HBD <- HBDsegments_rbind[which(HBDsegments_rbind$id==id & HBDsegments_rbind$famid==famid),]
-
+  
   if(nrow(HBD) == 0)
-    stop("No individual found")
+    if ((id %in% Submaps@submap_summary$id & famid %in% Submaps@submap_summary$famid ) == FALSE)
+      stop("No individual found, check spelling of id and famid")
+    else if (Submaps@submap_summary$quality[which(Submaps@submap_summary$id == id & Submaps@submap_summary$famid == famid)]<95)
+      stop("No HBD segment evaluation for this individual because of low QUALITY (<95)")
+    else if(Submaps@submap_summary$inbred[which(Submaps@submap_summary$id == id & Submaps@submap_summary$famid == famid)] == FALSE)
+      stop("Individual with good QUALITY (>95) but No HBD segment evaluation because he is not inbred")
   
   #regions options
   if (missing(regions)) 
@@ -51,7 +56,7 @@ plotHBDSegmentsId <- function(Submaps, unit= "cM", id, famid, regions, outfile, 
   else {
     outfile <- paste(outfile,".png",sep="") 
   }
-
+  
   plotSegmentsId(fileOrSubmaps=HBD, unit = unit, regions = myreg, main=paste("HBDsegments of", uniqueIds(famid, id)), build=build)
 }
 
